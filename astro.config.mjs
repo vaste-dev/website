3bf3002405e7bc28a2343b791b22cc849c581341
kieranwv <kieranwme@gmail.com>
2025-03-13 14:21:04 +0800

    refactor: remove unused dependencies and update logo paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,9 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel/serverless'
 import astroExpressiveCode from 'astro-expressive-code'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
   integrations: [
     tailwind(),
     astroExpressiveCode({
